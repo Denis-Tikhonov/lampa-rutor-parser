8c36c0b5365e82e5dab039cba6de440f91c02240
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-03-28 11:38:31 +0500

    Update worker.js
    stert

--- worker.js
+++ worker.js
@@ -270,10 +270,3 @@ function jsonResponse(data) {
     headers: { "Content-Type": "application/json; charset=utf-8", "Access-Control-Allow-Origin": "*" }
   });
 }
- } catch (e) {
-    return jsonResponse({ Results: [], Indexers: [], Message: e.message });
-  }
-  
-  return jsonResponse({ Results, Indexers: [], Message: "OK" });
-}
-}
